325232b3e46d8cfa4c24fb1ebe97bd32f7c670e8
Cris <kristiano.gon@gmail.com>
2025-05-29 09:46:12 -0300

    change: module federation config

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -2,6 +2,7 @@ import { createModuleFederationConfig } from "@module-federation/rsbuild-plugin"
 
 export default createModuleFederationConfig({
   name: "fiap_farm_inventory",
+  filename: "remoteEntry.js",
   exposes: {
     ".": "./src/components/InventoryComponent.tsx",
   },
